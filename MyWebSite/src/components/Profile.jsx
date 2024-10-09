d2e65527baed3a3dd9cba38422386d89ce576f34
Bahadır UYSAL <uysalbahadir1529@gmail.com>
2024-10-09 21:25:33 +0300

    Profile

--- MyWebSite/src/components/Profile.jsx
+++ MyWebSite/src/components/Profile.jsx
@@ -1,12 +1,44 @@
-
-
+import profil from "../assets/profil.png";
+import "./Profile.css";
 function Profile() {
-    return (
-      <>
-       
-      </>
-    );
-  }
-  
-  export default Profile;
-  
+  return (
+    <>
+      <div className="profile-container">
+        <h1>Profile</h1>
+        <div className="profile-content">
+          <div className="profile-info">
+            <h2>Basic Information</h2>
+            <span>
+              Doğum Tarihi <p>23.03.1995</p>
+            </span>
+            <span>
+              İkamet Şehri <p>İstanbul</p>
+            </span>
+            <span>
+              Eğitim Durumu <p>Hacettepe Ünv. Biyoloji Lisans, 2016</p>
+            </span>
+            <span>
+              Tercih Ettiği Rol<p>Frontend, UI</p>
+            </span>
+          </div>
+          <div className="profile-img">
+            <img src={profil} alt="" />
+          </div>
+          <div className="profile-about">
+            <h2>About Me</h2>
+            <p>
+              Lorem ipsum, dolor sit amet consectetur adipisicing elit. Veniam
+              aut, odit laborum aliquam voluptatum nisi mollitia.
+            </p>
+            <p>
+              Mnima accusamus ratione soluta aperiam sit voluptate? Dicta quod
+              deserunt quam temporibus cumque magnam!
+            </p>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default Profile;
