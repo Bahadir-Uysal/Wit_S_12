b8beb5a2f71f4a632e650efb3e76e8046e872cc9
Bahadır UYSAL <uysalbahadir1529@gmail.com>
2024-10-11 19:21:24 +0300

    data

---
+++ MyWebSite/src/datas/data.js
@@ -0,0 +1,111 @@
+export const languageData = {
+    en: {
+      introSection: {
+        heading: "I am a Frontend Developer",
+        subHeading:
+          "...who likes to craft solid and scalable frontend products with great user experiences.",
+        currentLang: "English",
+        darkMode: "dark mode",
+        lightMode: "light mode",
+      },
+      skillsSection: {
+        heading: "Skills",
+      },
+      profileSection: {
+        heading:"Profile",
+        basicInfoTitle: "Basic Information",
+        birthDate: "Birth Date",
+        birthDateValue: "23.03.1995",
+        city: "City of Residence",
+        cityValue: "Istanbul",
+        education: "Education",
+        educationValue: "AirForce NCO Collage, 2015",
+        preferredRole: "Preferred Role",
+        preferredRoleValue: "Frontend, UI",
+        aboutMeTitle: "About Me",
+        aboutMeText1: "I am open to learning new technologies, I enjoy solving problems and implementing my solutions. I aim to develop front-ends that appeal to both the eye and the user experience.",
+        aboutMeText2: "My biggest goal is to put my head together with my teammates during the creation process and improve myself by seeing my shortcomings.”",
+      },
+      projectsSection: {
+        heading: "Projects",
+        projects: [
+            {
+              img: "",
+              name: "MOVIE APP",
+              desc: "A movie app that I developed during my early learning processes.",
+              skills: ["react", "redux", "vercel"],
+              link: "https://pizzaorderform-b4q8iglhx-aysenayydins-projects.vercel.app",
+              github: "https://github.com/aysenayydin/pizzaorderform",
+            },
+            {
+              img: "",
+              name: "REDUX WATCHLIST",
+              desc: "One of the projects featured in this portfolio is the Favorite Movie Archive, built using Redux for state management. In this project, I implemented a watchlist feature where users can browse through movies, add their favorite movies to a watchlist, and manage this list efficiently.",
+              skills: ["react", "redux", "vercel"],
+              link: "https://fsweb-s10g3-redux-watchlist-solution-b747y5dii.vercel.app",
+              github: "https://github.com/aysenayydin/fsweb-s10g3-redux-watchlist-solution",
+            },
+          ],
+      },
+      footerSection: {
+        heading: "Send me a message!",
+        line1: "Got a question or proposal, or just want",
+      line2: "to say hello? Go ahead.",
+      }
+    },
+    tr: {
+      introSection: {
+        heading: "Frontend Developer...",
+        subHeading:
+          "... sağlam ve ölçeklenebilir, harika kullanıcı deneyimlerine sahip frontend ürünleri oluşturmayı seven biri.",
+        currentLang: "Türkçe",
+        darkMode: "karanlık tema",
+        lightMode: "aydınlık tema",
+      },
+      skillsSection: {
+        heading: "Yetenekler",
+      },
+      profileSection: {
+        heading:"Profil",
+        basicInfoTitle: "Temel Bilgiler",
+        birthDate: "Doğum Tarihi",
+        birthDateValue: "23.03.1995",
+        city: "İkamet Şehri",
+        cityValue: "İstanbul",
+        education: "Eğitim Durumu",
+        educationValue: "Milli Savunma Ünv. Elektronik Haberleşme, ÖnLisans, 2015",
+        preferredRole: "Tercih Ettiği Rol",
+        preferredRoleValue: "Frontend, UI",
+        aboutMeTitle: "Hakkımda",
+        aboutMeText1: "Yeni teknolojiler öğrenmeye açık, problem çözmekten ve çözümlerimi uygulamaktan keyif alıyorum. Hem göze hemde kullanıcı deneyimine hitap eden önyüzler geliştirmeyi hedefliyorum.",
+        aboutMeText2: "Yaratım sürecinde ekip arkadaşlarımla kafa kafaya vermek ve eksik yanlarımı görerek kendimi geliştirmek en büyük hedefim.",
+      },
+      projectsSection: {
+        heading: "Projeler",
+        projects: [
+            {
+              img: "",
+              name: "MOVIE APP",
+              desc: "İlk öğrenim süreçlerimde geliştridiğim bir film uygulaması.",
+              skills: ["react", "redux", "vercel"],
+              link: "https://pizzaorderform-b4q8iglhx-aysenayydins-projects.vercel.app",
+              github: "https://github.com/Bahadir-Uysal/Movie-App",
+            },
+            {
+              img: "",
+              name: "REDUX İZLEME LİSTESİ",
+              desc: "Bu portföyde yer alan projelerden biri, Favori Film Arşivi olup, Redux kullanılarak durum yönetimi sağlanmıştır. Bu projede, kullanıcıların filmler arasında gezinebileceği, favori filmlerini izleme listesine ekleyebileceği ve bu listeyi verimli bir şekilde yönetebileceği bir izleme listesi özelliği uyguladım.",
+              skills: ["react", "redux", "vercel"],
+              link: "https://fsweb-s10g3-redux-watchlist-solution-b747y5dii.vercel.app",
+              github: "https://github.com/aysenayydin/fsweb-s10g3-redux-watchlist-solution",
+            },
+          ],
+      },
+      footerSection: {
+        heading: "Bana bir mesaj gönderin!",
+        line1: "Bir sorunuz ya da teklifiniz mi var, yoksa sadece",
+      line2: "merhaba demek mi istiyorsunuz? Buyrun.",
+      },
+    },
+  };
+  
