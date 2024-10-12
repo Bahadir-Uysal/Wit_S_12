6e702d4b6694c46e96cdb436262e9cf701489cff
Bahadır UYSAL <uysalbahadir1529@gmail.com>
2024-10-12 03:10:28 +0300

    littlecss

--- MyWebSite/src/components/Hero.jsx
+++ MyWebSite/src/components/Hero.jsx
@@ -14,8 +14,8 @@ function Hero() {
             <h1>{texts.introSection.heading}</h1>
             <p>{texts.introSection.subHeading}</p>
             <div className="hero-buttons">
-              <button>Github</button>
-              <button>Linkedin</button>
+              <a href="https://github.com/Bahadir-Uysal"><i className="fa fa-github text-2xl" ></i>Github</a>
+              <a href="https://www.linkedin.com/in/bahadir-uysal/"> <i className="fa fa-linkedin"></i>Linkedin</a>
             </div>
           </div>
           <img src={myImage} className="hero-img" />
